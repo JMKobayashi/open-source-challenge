3dfec14e6141d2f89b12bf30b65c3d50abbc1419
JMKobayashi <james.m.kobayashi@gmail.com>
2021-04-16 19:50:12 -0300

    Cria o arquivo de teste e os testes com pytest.
    Cria testes para todas as funcionalidades do sistema.
    Importante rodar o teste com o banco vazio.

--- vough_backend/api/tests.py
+++ vough_backend/api/tests.py
@@ -1,3 +1,54 @@
-from django.test import TestCase
+import pytest
+import requests
+import json
 
-# Create your tests here.
+def test_empty_list_organizations():
+
+    response = requests.get('http://localhost:8000/api/orgs/')
+    assert response.status_code == 404
+
+
+@pytest.mark.parametrize(
+    'login,status',[
+        ('instruct-br',200), # Succes
+        ('adobe',200), # Succes
+        ('Netflix',200), # Succes
+        ('instruct',200), # Succes Nome vazio
+        ('twitter',200), # Succes
+        ('RedHatOfficial',200), # Succes
+        ('adsgser',404), # Error 404 Organização não encontrada
+        ('123456',404), # Error 404 Organização não encontrada
+        ('testestestestes',404), # Error 404 Organização não encontrada
+        ('fase5qwe',404) # Error 404 Organização não encontrada
+    ]
+)
+
+def test_get_organization(login,status):
+
+    response = requests.get('http://localhost:8000/api/orgs/'+login)
+    assert response.status_code == status
+
+def test_list_organization():
+
+    response = requests.get('http://localhost:8000/api/orgs/')
+    assert response.status_code == 200
+
+@pytest.mark.parametrize(
+    'login,status',[
+        ('instruct-br',204), # Succes
+        ('adobe',204), # Succes
+        ('Netflix',204), # Succes
+        ('instruct',204), # Succes Nome vazio
+        ('twitter',204), # Succes
+        ('RedHatOfficial',204), # Succes
+        ('adsgser',404), # Error 404 Organização não encontrada
+        ('123456',404), # Error 404 Organização não encontrada
+        ('testestestestes',404), # Error 404 Organização não encontrada
+        ('fase5qwe',404) # Error 404 Organização não encontrada
+    ]
+)
+
+def test_delete_organization(login,status):
+
+    response = requests.delete('http://localhost:8000/api/orgs/'+login)
+    assert response.status_code == status
